780a1b2d18e72616624e2ba12c7778ae3fa1a2f6
Heiko Lige <helige@ttu.ee>
2021-06-01 17:51:21 +0300

    Add project files.

---
+++ CyberCube/CyberCubeTask1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CyberCube
+{
+    public class CyberCubeTask1
+    {
+        public static void AddExtraLetter()
+        {
+            var random = new Random();
+            var chars = "abcdefghijklmnopqrstuvwxyz";
+            var stringChars = new char[random.Next(1, 10)];
+            stringChars[0] = chars[random.Next(chars.Length)];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var s = new String(stringChars);
+            var t = s + stringChars[0];
+            var output = stringChars[0];
+            Console.WriteLine("s= " + s);
+            Console.WriteLine("t= " + t);
+            Console.WriteLine("The added letter: " + output);
+        }
+    }
+}
---
+++ CyberCube/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CyberCube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CyberCubeTask1 Task1 = new CyberCubeTask1();
+            CyberCubeTask1.AddExtraLetter();
+        }
+    }
+}
